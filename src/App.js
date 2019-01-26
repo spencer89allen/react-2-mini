579d7db696d4c810133271fe935a9c7d122ea65e
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-01-26 11:20:34 -0700

    steps 1 through 6 completed

--- src/App.js
+++ src/App.js
@@ -9,26 +9,75 @@ import TextContainer from './components/TextContainer';
 
 class App extends Component {
   // constructor
+  constructor() {
+    super()
+
+    this.state = {
+      fontColor: 'black',
+      fontSize: 12,
+      fontFamily: 'monospace',
+      allowEdit: 'true',
+    }
+
+    this.handleUpdateColor = this.handleUpdateColor.bind(this)
+    this.handleUpdateSize = this.handleUpdateSize.bind(this)
+    this.handleUpdateFamily = this.handleUpdateFamily.bind(this)
+    this.handleUpdateEditStatus = this.handleUpdateEditStatus.bind(this)
+
+  }
 
   // updateColor
+  handleUpdateColor(val){
+    this.setState({
+      fontColor: val,
+    })
+  }
 
   // updateSize
+  handleUpdateSize(val) {
+    this.setState({
+      fontSize: val,
+    })
+  }
 
   // updateFamily
+  handleUpdateFamily(val) {
+    this.setState({
+      fontFamily: val,
+    })
+  }
 
   // updateEditStatus
+  handleUpdateEditStatus(val) {
+    this.setState({
+      allowEdit: val,
+    })
+  }
 
   render() {
     return (
       <div>
         <div className="headerBar">
+
           { /* Render EditToggle */ }
+          <EditToggle update={this.handleUpdateEditStatus}></EditToggle>
+
           { /* Render ColorChanger */ }
+          <ColorChanger update={this.handleUpdateColor} allowEdit={this.state.allowEdit}></ColorChanger>
+
           { /* Render SizeChanger */ }
+          <SizeChanger update={this.handleUpdateSize} allowEdit={this.state.allowEdit}></SizeChanger>
+
           { /* Render FamilyChanger */ }
+          <FamilyChanger update={this.handleUpdateFamily} allowEdit={this.state.allowEdit}></FamilyChanger>
+
+
         </div>
         <div className="textArea">
+
           { /* Render TextContainer */ }
+          <TextContainer fontColor={this.state.fontColor} fontSize={this.state.fontSize} fontFamily={this.state.fontFamily}></TextContainer>
+
         </div>
       </div>
     )
--- src/components/ColorChanger.js
+++ src/components/ColorChanger.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function ColorChanger() {
+export default function ColorChanger(props) {
+  console.log('Color Changer Prpops:', props)
   return (
-    <select className="dropDownContainer">
+    <select className="dropDownContainer" onChange={ e => props.update(e.target.value)} disabled={props.allowEdit === "false" }>
       <option value="black"> Black </option>
       <option value="blue"> Blue </option>
       <option value="green"> Green </option>
